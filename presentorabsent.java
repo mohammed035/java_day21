54cbca1755eb1eb92ff3259ce7453ee8e2eb15e3
mohammed035 <mohammedabbas035@gmail.com>
2020-08-08 17:40:07 +0530

    employee is present or absent

---
+++ presentorabsent.java
@@ -0,0 +1,12 @@
+public class presentorabsent
+{
+                        public static void main(String[]args)
+                        {
+                        int IS_PRESENT=0;
+                   double empCheck=Math.floor(Math.random() * 10) % 2;
+                        if (empCheck == IS_PRESENT)
+                 System.out.println("employee present");
+         else
+                 System.out.println("employee absent");
+        }
+}
